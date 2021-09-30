9b4e5e23b877b042f31fd2d39e3c973877e27529
cugarte94 <cugarte@guiasyscoutschile.cl>
2021-09-29 23:08:51 -0300

    clase 8

--- clase8/main.py
+++ clase8/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
 
 @app.route('/')
 def index():
@@ -20,19 +22,33 @@ def signup():
 
 @app.route('/panel')
 def dashboard():
-    return render_template('dashboard.html')
+    if 'active' in session:
+        return render_template('dashboard.html')
+    return redirect('/')
 
-@app.route('/users', methods=['POST'])
+@app.route('/check-user', methods=['POST'])
 def create_user():
-    print("Got Post Info")
-    print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
-    return render_template(
-        "show.html",
-        name_on_template=name_from_form,
-        email_on_template=email_from_form
-    )
+    users = [
+        {'name' : 'John Shelby', 'email' : 'john@shelby.com' , 'password' : 'johnshelby'},
+        {'name' : 'Tommy Shelby', 'email' : 'tommy@shelby.com' , 'password' : 'tommyshelby'},
+        {'name' : 'Arthur Shelby', 'email' : 'arthur@shelby.com' , 'password' : 'arthurshelby'},
+        {'name' : 'Finn Shelby', 'email' : 'finn@shelby.com' , 'password' : 'finnshelby'},
+        {'name' : 'Michael Gray', 'email' : 'michael@gray.com' , 'password' : 'michaelgray'},
+    ]
+
+    for user in users:
+        if user['email'] == request.form['email'] and user['password'] == request.form['pass']:
+            session['name'] = user['name']
+            session['active'] = True
+            return redirect('/panel')
+    flash('Error en Email o Clave')
+    return redirect("/ingresar")	
+
+@app.route('/salir')
+def logout():
+    session.clear()
+    return redirect('/')
+
 
 
 if __name__=="__main__":
